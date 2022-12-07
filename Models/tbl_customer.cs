90beb2b6e36c23383f0cf16ce40b42313cef1dd1
hiro-suzuki-pe <suzuki.hiroyuki@mbr.nifty.com>
2022-12-07 14:59:09 +0900

    初期データ作成中

--- Models/tbl_customer.cs
+++ Models/tbl_customer.cs
@@ -12,11 +12,11 @@
         public string address { get; set; } = String.Empty;
         public string tel { get; set; } = String.Empty;
         public int staffId { get; set; }
-        public DateTime first_action_date { get; set; }
+        public DateTime? first_action_date { get; set; }
         public string memo { get; set; } = String.Empty;
-        public DateTime input_date { get; set; }
+        public DateTime? input_date { get; set; }
         public string input_staff_name { get; set; } = String.Empty;
-        public DateTime update_date { get; set; }
+        public DateTime? update_date { get; set; }
         public string update_staff_name { get; set; } = String.Empty;
         public bool delete_flag { get; set; }
     }
