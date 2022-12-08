0d34252faf7082494a401cf91bb0dd5ee4e8b100
hiro-suzuki-pe <suzuki.hiroyuki@mbr.nifty.com>
2022-12-08 12:00:24 +0900

    CustomerのSeedDataを実装。

--- Models/SeedData.cs
+++ Models/SeedData.cs
@@ -31,190 +31,195 @@ public static class SeedData
             }
 
             context.Customer.AddRange(
-                new Customer {
+                new tbl_customer {
                     customer_name = "宮崎典子",
                     customer_kana = "ミヤザキノリコ",
                     companyId = 1,
-                    section = 総務部,
+                    section = "総務部",
                     post = "部長",
                     zipcode = "300-1261",
                     address = "つくば市あしび野",
-                    tel = 5029,
+                    tel = "03 - 727 - 8488",
                     staffId = 1,
-                    first_action_date = 44562,
+                    first_action_date = DateTime.Parse("2022/6/15"),
                     memo = "メモ1",
-                    input_date = 44532,
+                    input_date = DateTime.Parse("2022/6/15"),
                     input_staff_name = "桐生祥秀",
-                    update_date = 44532,
+                    update_date = DateTime.Parse("2022/6/15"),
                     update_staff_name = "桐生祥秀",
                     delete_flag = false
                 },
-                new Customer
+                new tbl_customer
                 {
                     customer_name = "尾松佳美",
                     customer_kana = "オマツヨシミ",
                     companyId = 3,
-                    section = 設計部,
+                    section = "設計部",
                     post = "担当",
                     zipcode = "300-0341",
                     address = "稲敷郡阿見町うずら野",
-                    tel = 5017,
+                    tel = "03 - 528 - 3711",
                     staffId = 3,
-                    first_action_date = 44702 
-                    memo = "メモ3" 
-                    input_date = 44672,
-                    input_staff_name = "松本尚子"
-                    update_date = 44672 
-                    update_staff_name = "松本尚子"  
+                    first_action_date = DateTime.Parse("2020/1/1"),
+                    memo = "メモ3",
+                    input_date = DateTime.Parse("2020/1/1"),
+                    input_staff_name = "松本尚子",
+                    update_date = DateTime.Parse("2020/1/1"),
+                    update_staff_name = "松本尚子",
                     delete_flag = false
                 },
-                new Customer
+                new tbl_customer
                 {
-                    customer_name = "佐賀陽" 
-                    customer_kana = "サガヨウ"  
-                    companyId = 2
-                    section = 製造部
-                    post = "課長"  
-                    zipcode = "311-2425" 
-                    address = "潮来市あやめ"   
-                    tel = 5023   
-                    staffId = 2  
-                    first_action_date = 44632 
-                    memo = "メモ2" 
-                    input_date = 44602  
-                    input_staff_name = "足立美由紀"  
-                    update_date = 44602
-                    update_staff_name = "足立美由紀" 
+                    customer_name = "佐賀陽",
+                    customer_kana = "サガヨウ",  
+                    companyId = 2,
+                    section = "製造部",
+                    post = "課長",
+                    zipcode = "311-2425",
+                    address = "潮来市あやめ",
+                    tel = "03 - 6216 - 5268",
+                    staffId = 2,
+                    first_action_date = DateTime.Parse("2020/1/15"),
+                    memo = "メモ2",
+                    input_date = DateTime.Parse("2020/1/15"),
+                    input_staff_name = "足立美由紀",
+                    update_date = DateTime.Parse("2020/1/15"),
+                    update_staff_name = "足立美由紀",
                     delete_flag = false
                 },
-                new Customer
+                new tbl_customer
                 {
-                    customer_name = "本塩遼" 
-                    customer_kana = "ホンシオリョウ"
-                    companyId = 4 section = QA部
-                    post = "主任"  
-                    zipcode = "300-0028" 
-                    address = "土浦市おおつ野"   
-                    tel = 5011    staffId = 1 
-                    first_action_date = 44772
-                    memo = "メモ4"  
-                    input_date = 44742   
-                    input_staff_name = "桐生祥秀" 
-                    update_date = 44742  
-                    update_staff_name = "桐生祥秀"   
-                    delete_flag = false
-                },
-
-                new Customer
-                {
-                    customer_name = "谷掛玲子"  
-                    customer_kana = "タニガケレイコ" 
-                    companyId = 1
-                    section = 総務部
-                    post = "パート" 
-                    zipcode = "319-1417"  
-                    address = "日立市かみあい町" 
-                    tel = 5005  
-                    staffId = 2  
-                    first_action_date = 44842
-                    memo = "メモ5" 
-                    input_date = 44812  
-                    input_staff_name = "足立美由紀"  
-                    update_date = 44812  
-                    update_staff_name = "足立美由紀" 
+                    customer_name = "本塩遼",
+                    customer_kana = "ホンシオリョウ",
+                    companyId = 4,
+                                        section = "QA部",
+                    post = "主任",
+                    zipcode = "300-0028",
+                    address = "土浦市おおつ野",
+                    tel = "03 - 6488 - 949",
+                    staffId = 1, 
+                    first_action_date = DateTime.Parse("2020/1/29"),
+                    memo = "メモ4",
+                    input_date = DateTime.Parse("2020/1/29"),
+                    input_staff_name = "桐生祥秀",
+                    update_date = DateTime.Parse("2020/1/29"),
+                    update_staff_name = "桐生祥秀",
                     delete_flag = false
                 },
 
-                new Customer
+                new tbl_customer
                 {
-                    customer_name = "近藤晃"
-                    customer_kana = "コンドウアキラ"
-                    companyId = 2
-                    section = 設計部 
-                    post = "部長"  
-                    zipcode = "300-2668"  
-                    address = "つくば市かみかわ" 
-                    tel = 4999  
-                    staffId = 3  
-                    first_action_date = 44912 
-                    memo = "メモ6" 
-                    input_date = 44882   
-                    input_staff_name = "松本尚子" 
-                    update_date = 44882  
-                    update_staff_name = "松本尚子"   
+                    customer_name = "谷掛玲子",
+                    customer_kana = "タニガケレイコ",
+                    companyId = 1,
+                    section = "総務部",
+                    post = "パート",
+                    zipcode = "319-1417",
+                    address = "日立市かみあい町",
+                    tel = "03 - 8225 - 3243",
+                    staffId = 2,
+                    first_action_date = DateTime.Parse("2020/4/22"),
+                    memo = "メモ5",
+                    input_date = DateTime.Parse("2020/4/22"),
+                    input_staff_name = "足立美由紀",
+                    update_date = DateTime.Parse("2020/4/22"),
+                    update_staff_name = "足立美由紀",
                     delete_flag = false
                 },
-                new Customer
+
+                new tbl_customer
                 {
-                    customer_name = "福田美紀"  
-                    customer_kana = "フクダミキ" 
-                    companyId = 3 
-                    section = 製造部
-                    post = "課長"  
-                    zipcode = "310-0842" 
-                    address = "水戸市けやき台"  
-                    tel = 4993   
-                    staffId = 1  
-                    first_action_date = 44252 
-                    memo = "メモ7" 
-                    input_date = 44952  
-                    input_staff_name = "桐生祥秀"
-                    update_date = 44952  
-                    update_staff_name = "桐生祥秀"  
+                    customer_name = "近藤晃",
+                    customer_kana = "コンドウアキラ",
+                    companyId = 2,
+                    section = "設計部", 
+                    post = "部長",
+                    zipcode = "300-2668",
+                    address = "つくば市かみかわ",
+                    tel = "03 - 8071 - 1103",
+                    staffId = 3,
+                    first_action_date = DateTime.Parse("2020/4/8"),
+                    memo = "メモ6",
+                    input_date = DateTime.Parse("2020/4/8"),
+                    input_staff_name = "松本尚子",
+                    update_date = DateTime.Parse("2020/4/8"),
+                    update_staff_name = "松本尚子",
                     delete_flag = false
                 },
-                new Customer
+                new tbl_customer
                 {
-                    customer_name = "関口裕太"
-                    customer_kana = "セキグチユウタ"
-                    companyId = 4
-                    section = QA部 
-                    post = "担当"  
-                    zipcode = "302-0128"  
-                    address = "守谷市けやき台"  
-                    tel = 4987   
-                    staffId = 2   
-                    first_action_date = 44322
-                    memo = "メモ8" 
-                    input_date = 45022  
-                    input_staff_name = "足立美由紀"  
-                    update_date = 45022 
-                    update_staff_name = "足立美由紀"  
+                    customer_name = "福田美紀",
+                    customer_kana = "フクダミキ",
+                    companyId = 3,
+                    section = "製造部",
+                    post = "課長",
+                    zipcode = "310-0842",
+                    address = "水戸市けやき台",
+                    tel = "03 - 6978 - 8883",
+                    staffId = 1,
+                    first_action_date = DateTime.Parse("2020/3/25"),
+                    memo = "メモ7",
+                    input_date = DateTime.Parse("2020/3/25"),
+                    input_staff_name = "桐生祥秀",
+                    update_date = DateTime.Parse("2020/3/25"),
+                    update_staff_name = "桐生祥秀",
                     delete_flag = false
                 },
-                new Customer
+                new tbl_customer
                 {
-                    customer_name = "鳥内啓子"  
-                    customer_kana = "トリウチケイコ" 
-                    companyId = 9
-                    section = 設計部
-                    post = "主任"  
-                    zipcode = "306-0055"  
-                    address = "古河市けやき平"   
-                    tel = 4981   
-                    staffId = 3  
-                    first_action_date = 44392
-                    memo = "メモ9"  input_date =45092   
-                    input_staff_name = "松本尚子" 
-                    update_date = 45092   
-                    update_staff_name = "松本尚子" 
+                    customer_name = "関口裕太",
+                    customer_kana = "セキグチユウタ",
+                    companyId = 4,
+                    section = "QA部", 
+                    post = "担当",
+                    zipcode = "302-0128",
+                    address = "守谷市けやき台",
+                    tel = "03 - 2793 - 1778",
+                    staffId = 2,
+                    first_action_date = DateTime.Parse("2020/3/11"),
+                    memo = "メモ8",
+                    input_date = DateTime.Parse("2020/3/11"),
+                    input_staff_name = "足立美由紀",
+                    update_date = DateTime.Parse("2020/3/11"),
+                    update_staff_name = "足立美由紀",
                     delete_flag = false
                 },
-                new Customer
+                new tbl_customer
                 {
-                    customer_name = "辰巳新" 
-                    customer_kana = "タツミアラタ"  
-                    companyId = 10    
-                    section = 製造部 
-                    post = "パート" 
-                    zipcode = "306-0308" 
-                    address = "猿島郡五霞町ごかみらい"  
-                    tel = 4975    staffId = 1   
-                    first_action_date = 44462 
-                    memo = "メモ10" input_date = 45162  
-                    input_staff_name = "桐生祥秀" 
-                    update_date = 45162  
-                    update_staff_name = "桐生祥秀"   
+                    customer_name = "鳥内啓子",
+                    customer_kana = "トリウチケイコ",
+                    companyId = 9,
+                    section = "設計部",
+                    post = "主任",
+                    zipcode = "306-0055",
+                    address = "古河市けやき平",
+                    tel = "03 - 241 - 2105",
+                    staffId = 3,
+                    first_action_date = DateTime.Parse("2020/2/26"),
+                    memo = "メモ9",
+                    input_date = DateTime.Parse("2020/2/26"),
+                    input_staff_name = "松本尚子",
+                    update_date = DateTime.Parse("2020/2/26"),
+                    update_staff_name = "松本尚子",
+                    delete_flag = false
+                },
+                new tbl_customer
+                {
+                    customer_name = "辰巳新",
+                    customer_kana = "タツミアラタ",
+                    companyId = 10,
+                    section = "製造部", 
+                    post = "パート",
+                    zipcode = "306-0308",
+                    address = "猿島郡五霞町ごかみらい",
+                    tel = "03 - 5277 - 8160",
+                    staffId = 1,
+                    first_action_date = DateTime.Parse("2020/2/12"),
+                    memo = "メモ10",
+                    input_date = DateTime.Parse("2020/2/12"),
+                    input_staff_name = "桐生祥秀",
+                    update_date = DateTime.Parse("2020/2/12"),
+                    update_staff_name = "桐生祥秀",
                     delete_flag = false
                 }
             );
--- Program.cs
+++ Program.cs
@@ -10,6 +10,13 @@ builder.Services.AddDbContext<MyContext>(options =>
 
 var app = builder.Build();
 
+using (var scope = app.Services.CreateScope())
+{
+    var services = scope.ServiceProvider;
+
+    SeedData.Initialize(services);
+}
+
 // Configure the HTTP request pipeline.
 if (!app.Environment.IsDevelopment())
 {
