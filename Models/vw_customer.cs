f860a4d92a021962c14ce6acb8b3683e1786d204
Hiroyuki Suzuki <suzuki.hiroyuki@mbr.nifty.com>
2022-12-10 16:57:40 +0900

    顧客詳細画面まで完了

--- Models/vw_customer.cs
+++ Models/vw_customer.cs
@@ -1,13 +1,24 @@
-﻿namespace CustomerManagementSystem.Models
+﻿using static Microsoft.EntityFrameworkCore.DbLoggerCategory.Database;
+
+namespace CustomerManagementSystem.Models
 {
     public class vw_customer
     {
         public int Id { get; set; }
         public string customer_name { get; set; } = String.Empty;
         public string customer_kana { get; set; } = String.Empty;
-        public string company_name { get; set; } = String.Empty;
         public string section { get; set; } = String.Empty;
         public string post { get; set; } = String.Empty;
+        public DateTime first_action_date { get; set; }
+        public string zipcode { get; set; } = String.Empty;
+        public string address { get; set; } = String.Empty;
+        public string tel { get; set; } = String.Empty;
+        public string memo { get; set; } = String.Empty;
+        public DateTime	update_date { get; set; }
+        public string update_staff_name { get; set; } = String.Empty;
+        public string company_name { get; set; } = String.Empty;
+        public string company_kana { get; set; } = String.Empty;
         public string staff_name { get; set; } = String.Empty;
     }
 }
+
