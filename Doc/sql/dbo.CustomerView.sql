8349ea613b25d79805764f003744799b785b7e26
hiro-suzuki-pe <suzuki.hiroyuki@mbr.nifty.com>
2022-12-20 11:43:14 +0900

    営業報告書一覧の項目を日本語化した

---
+++ Doc/sql/dbo.CustomerView.sql
@@ -0,0 +1,9 @@
+﻿CREATE VIEW [dbo].[CustomerView]
+	AS SELECT Customer.Id, customer_name, customer_kana, section, post, 
+		first_action_date, zipcode, Customer.address, tel, memo,
+		update_date, update_staff_name,
+		Company.company_name, Company.company_kana, 
+		Staff.staff_name
+		FROM [Customer]
+			INNER JOIN Staff on Customer.staffId = Staff.Id
+			INNER JOIN Company on Customer.companyId = Company.Id
