a1c64cb4387ff2d5c0369509ecc5f398a4a7c7e3
hiro-suzuki-pe <suzuki.hiroyuki@mbr.nifty.com>
2022-12-20 13:36:47 +0900

    日付によるフィルタ機能を追加

--- Pages/SalesReport/Index.cshtml.cs
+++ Pages/SalesReport/Index.cshtml.cs
@@ -12,23 +12,41 @@ namespace CustomerManagementSystem.Pages.SalesReport
     public class IndexModel : PageModel
     {
         private readonly CustomerManagementSystem.Models.MyContext _context;
+        private readonly IConfiguration Configuration;
 
-        public IndexModel(CustomerManagementSystem.Models.MyContext context)
+        public IndexModel(CustomerManagementSystem.Models.MyContext context, IConfiguration configuration)
         {
             _context = context;
+            Configuration = configuration;
         }
 
         public IList<vw_salesReport> vw_salesReport { get;set; } = default!;
 
-        public async Task OnGetAsync()
+        public async Task OnGetAsync(string fromDate, string toDate)
         {
             IQueryable<vw_salesReport> salesReportIQ = from s in _context.SalesReport select s;
 
-            if (_context.SalesReport != null)
+            if (_context.SalesReport == null)
+                return;
+
+            DateTime dt1, dt2;
+            if (DateTime.TryParse(fromDate, out dt1) && DateTime.TryParse(toDate, out dt2))
             {
-                salesReportIQ = salesReportIQ.OrderBy(s => s.action_date);
-                vw_salesReport = await salesReportIQ.AsNoTracking().ToListAsync();
+                salesReportIQ =
+                    salesReportIQ.Where(s => s.action_date >= dt1 && s.action_date <= dt2);
             }
+            else if (DateTime.TryParse(fromDate, out dt1))
+            {
+                salesReportIQ =
+                    salesReportIQ.Where(s => s.action_date >= dt1);
+            }
+            else if (DateTime.TryParse(toDate, out dt2))
+            {
+                salesReportIQ =
+                    salesReportIQ.Where(s => s.action_date <= dt2);
+            }
+            salesReportIQ = salesReportIQ.OrderBy(s => s.action_date);
+            vw_salesReport = await salesReportIQ.AsNoTracking().ToListAsync();
         }
     }
 }
