37256c6aa93b1e2b21c31592429b23aea6470578
hiro-suzuki-pe <suzuki.hiroyuki@mbr.nifty.com>
2023-01-20 16:14:23 +0900

    営業情報の新規登録，まだ登録ページに届かない。

--- Pages/Action/Create.cshtml.cs
+++ Pages/Action/Create.cshtml.cs
@@ -6,6 +6,7 @@ using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using Microsoft.AspNetCore.Mvc.Rendering;
 using CustomerManagementSystem.Models;
+using Microsoft.EntityFrameworkCore;
 
 namespace CustomerManagementSystem.Pages.Action
 {
@@ -24,21 +25,91 @@ namespace CustomerManagementSystem.Pages.Action
         }
 
         [BindProperty]
-        public tbl_action tbl_action { get; set; }
-        
+        public tbl_action tbl_action { get; set; } = default!;
+        public tbl_customer tbl_customer { get; set; } = default!;
+        public tbl_company tbl_company { get; set; } = default!;
+        public tbl_staff tbl_staff { get; set; } = default!;
+
+        public SelectList? StaffSL { get; set; }
+        public void PopulateStaffDropDownList(MyContext _context,
+            object selectedStaff = null)
+        {
+            var staffQuery = from d in _context.Staff
+                             orderby d.Id // Sort by staff_name.
+                             select d;
+
+            StaffSL = new SelectList(staffQuery, // items       
+                        "Id",                   // dataValueField
+                        "staff_name",           // dataTextField
+                        selectedStaff);         // selectedValue
+        }
+
+        public async Task<IActionResult> OnGetAsync(int? id)        // id:  customer ID (NOT action ID)
+        {
+            tbl_customer = await _context.Customer.FirstOrDefaultAsync(m => m.Id == id);
+            if (tbl_customer == null)
+            {
+                return NotFound();
+            }
+
+            tbl_company = await _context.Company.FirstOrDefaultAsync(m => m.Id == tbl_customer.companyId);
+            if (tbl_company == null)
+            {
+                return NotFound();
+            }
+
+            tbl_staff = await _context.Staff.FirstOrDefaultAsync(m => m.Id == tbl_action.action_staffId);
+            if (tbl_staff == null)
+            {
+                return NotFound();
+            }
+
+            PopulateStaffDropDownList(_context);
+            return Page();
+        }
 
         // To protect from overposting attacks, see https://aka.ms/RazorPagesCRUD
-        public async Task<IActionResult> OnPostAsync()
+        public async Task<IActionResult> OnPostAsync(string action_button, int id)
         {
-          if (!ModelState.IsValid)
+            tbl_customer = await _context.Customer.FirstOrDefaultAsync(m => m.Id == tbl_action.customerId);
+            if (tbl_customer == null)
+            {
+                return NotFound();
+            }
+
+            if (action_button == "キャンセル")
+            {
+                //           return RedirectToPage("./Index");
+                return RedirectToPage("../Customer/Details", new { id = tbl_customer.Id });
+            }
+
+            if (!ModelState.IsValid)
             {
                 return Page();
             }
 
             _context.Action.Add(tbl_action);
-            await _context.SaveChangesAsync();
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!tbl_actionExists(tbl_action.Id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
 
-            return RedirectToPage("./Index");
+            return RedirectToPage("../Customer/Details", new { id = tbl_customer.Id });
+        }
+        private bool tbl_actionExists(int id)
+        {
+            return _context.Action.Any(e => e.Id == id);
         }
     }
 }
