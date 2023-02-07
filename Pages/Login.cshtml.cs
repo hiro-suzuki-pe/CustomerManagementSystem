110d666c50a2eaff4b9405269273e628e4b79209
hiro-suzuki-pe <suzuki.hiroyuki@mbr.nifty.com>
2023-02-07 11:24:43 +0900

    Cookie認証，というかログイン情報をCookieに残す処理ができたように思う。

--- Pages/Login.cshtml.cs
+++ Pages/Login.cshtml.cs
@@ -1,7 +1,10 @@
 using CustomerManagementSystem.Models;
+using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Authentication.Cookies;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using Microsoft.EntityFrameworkCore;
+using System.Security.Claims;
 using System.Security.Cryptography;
 
 namespace CustomerManagementSystem.Pages
@@ -27,12 +30,81 @@ namespace CustomerManagementSystem.Pages
         {
         }
 
-        public async Task<IActionResult> OnPostAsync(string StaffUserId, string StaffPassword)
+        /*
+         * private string AuthenticateUser(string UserId, string Password)
+               {
+                   var staff = await _context.Staff.FirstOrDefaultAsync(m => m.userId == UserId);
+
+                   if (staff == null)
+                   {
+                       return null;
+                   }
+                   if (staff.password != Password)
+                   {
+                       return null;
+                   };
+                   return staff.userId;
+               }
+       */
+//        public async Task<IActionResult> OnPostAsync(string StaffUserId, string StaffPassword,
+//            string ReturnUrl = null)
+       public async Task<IActionResult> OnPostAsync(string StaffUserId, string StaffPassword)
         {
             if (!ModelState.IsValid)
             {
                 return Page();
             }
+
+            //            var user = await AuthenticateUser(StaffUserId, StaffPassword);
+            var user = await _context.Staff.FirstOrDefaultAsync(m => m.userId == StaffUserId);
+            if (user == null || user.password != StaffPassword)
+            {
+                ModelState.AddModelError(string.Empty, "ÉçÉOÉCÉìÇÕé∏îsÇµÇ‹ÇµÇΩÅB");
+                return Page();
+            }
+
+            var claims = new List<Claim>
+            {
+                new Claim(ClaimTypes.Name, StaffUserId),
+                new Claim("Password", StaffPassword),
+                new Claim(ClaimTypes.Role, "Administrator")
+            };
+
+            var ClaimsIdentity = new ClaimsIdentity(
+                claims,  CookieAuthenticationDefaults.AuthenticationScheme);
+
+            var authProperties = new AuthenticationProperties
+            {
+                //AllowRefresh = <bool>,
+                // Refreshing the authentication session should be allowed.
+
+                //ExpiresUtc = DateTimeOffset.UtcNow.AddMinutes(10),
+                // The time at which the authentication ticket expires. A 
+                // value set here overrides the ExpireTimeSpan option of 
+                // CookieAuthenticationOptions set with AddCookie.
+
+                //IsPersistent = true,
+                // Whether the authentication session is persisted across 
+                // multiple requests. When used with cookies, controls
+                // whether the cookie's lifetime is absolute (matching the
+                // lifetime of the authentication ticket) or session-based.
+
+                //IssuedUtc = <DateTimeOffset>,
+                // The time at which the authentication ticket was issued.
+
+                //RedirectUri = <string>
+                // The full path or absolute URI to be used as an http 
+                // redirect response value.
+            };
+            /*
+            {
+                AllowRefresh = true,
+                ExpiresUtc = DateTimeOffset.UtcNow.AddMinutes(10),
+                IsPersistent = true,
+                IssuedUtc = DateTime.Now,       // <DateTimeOffset>,
+                RedirectUri = "string",
+            };
+            */
 
             var tbl_staffL = await _context.Staff.FirstOrDefaultAsync(m => m.userId == StaffUserId);
 
